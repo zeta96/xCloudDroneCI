07462f8d06623a8082ab9ef7a5ecbda0bac47a6e
zeta96 <ahmad.zainul857@gmail.com>
2022-02-24 14:48:07 +0700

    test

--- kernel_build_gcc.sh
+++ kernel_build_gcc.sh
@@ -82,10 +82,10 @@ make -j$(nproc) ARCH=arm64 O=out \
     AR=${GCC64_ROOTDIR}/bin/aarch64-elf-ar \
     AS=${GCC64_ROOTDIR}/bin/aarch64-elf-as \
 #   NM=${GCC64_ROOTDIR}/bin/aarch64-elf-nm \
-    NM=${GCC64_ROOTDIR}/bin/llvm-nm \
+#    NM=${GCC64_ROOTDIR}/bin/llvm-nm \
     CC=${GCC64_ROOTDIR}/bin/aarch64-elf-gcc \
 #   OBJCOPY=${GCC64_ROOTDIR}/bin/aarch64-elf-objcopy \
-    OBJCOPY=${GCC64_ROOTDIR}/bin/llvm-objcopy \
+#    OBJCOPY=${GCC64_ROOTDIR}/bin/llvm-objcopy \
     OBJDUMP=${GCC64_ROOTDIR}/bin/aarch64-elf-objdump \
     OBJSIZE=${GCC64_ROOTDIR}/bin/aarch64-elf-size \
     READELF=${GCC64_ROOTDIR}/bin/aarch64-elf-readelf \
